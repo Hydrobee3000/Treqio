9828b7cd37d828d6af840fb7963e2132b4569de1
Заикин Алексей <av.zaikin2@severstal.com>
2026-04-28 19:46:48 +0200

    add code quality tools

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**', '**/coverage/**'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+)
